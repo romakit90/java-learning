0edfbf9b259f0541aa4248fc7c5bcf8a4b5f3670
Romans Kitcenko <romakit90@gmail.com>
2026-06-09 13:31:51 +0300

    Added lesson 30

---
+++ src/lesson30/collectionsmethods/games/GameController.java
@@ -0,0 +1,12 @@
+package lesson30.collectionsmethods.games;
+
+import lesson30.collectionsmethods.games.poker.PokerGame;
+
+public class GameController {
+
+    public static void main(String[] args) {
+
+        PokerGame fiveCardDraw = new PokerGame(8, 5);
+        fiveCardDraw.startPlay();
+    }
+}
